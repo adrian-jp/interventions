09f13979ad12a3c0631aac458df9a2e3d7a812cb
Adri <adrianjose.jossepaupion2019@campus-eni.fr>
2020-03-06 18:43:37 +0100

    Démarrage du projet

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,30 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { InterventionTableComponent } from './intervention-table/intervention-table.component';
+import { NewInterventionComponent } from './new-intervention/new-intervention.component';
+import { EditInterventionComponent } from './edit-intervention/edit-intervention.component';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {RouterModule, Routes} from '@angular/router';
+import {InterventionService} from './services/intervention.service';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    InterventionTableComponent,
+    NewInterventionComponent,
+    EditInterventionComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
-  providers: [],
+  providers: [
+    InterventionService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
